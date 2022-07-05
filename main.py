b7269857755ed863f4959af38fb82496fc9f7797
CECI\63791 <leo_812leo@hotmail.com.tw>
2022-07-05 17:59:48 +0800

    feature: rotation

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ Created on Wed Oct 27 16:53:59 2021
 @author: 63791
 """
 import numpy as np
-from function import Newton,unit_vector,num,sign_convention
+from function import Newton,unit_vector,num,sign_convention,Newton2d
+
 from itertools import chain,accumulate,product
 from fig import plot_figure
 import pandas as pd
 import matplotlib.pyplot as plt
-#class Joints (chord,brace,tw)
+
 
 class Joints():
 # =============================================================================
@@ -22,6 +23,10 @@ class Joints():
         self.N = N
         self.tw = tw
         self.df = ''
+        a,b, self.R, self.T = self.chord.values()
+        c,d, self.r, self.t = self.brace.values()
+        self.a, self.b, self.c, self.d = np.array([a,b,c,d])
+        
         
     def to_excel(self):  
         
@@ -47,9 +52,8 @@ class Joints():
                     break
             return xx
         data = []  
-        a,b,R,T = self.chord.values()
-        c,d,r,t = self.brace.values()
-        a,b,c,d = np.array([a,b,c,d])
+        a, b, c, d =  self.a, self.b, self.c, self.d
+        R, T, r, t =  self.R, self.T, self.r, self.t
         #座標單位向量
         Uc = unit_vector(b-a)                               #chord 中心線
         U_gamma = unit_vector(d-c)                          #brace 中心線
@@ -169,35 +173,75 @@ class Joints():
         if self.df=='':
             self.to_excel()
         df = self.df
-        print(df)
-        a,b,R,T = self.chord.values()
-        c,d,r,t = self.brace.values()
-        a,b,c,d = np.array([a,b,c,d])           
+        a, b, c, d =  self.a, self.b, self.c, self.d
+        R, r  =  self.R, self.r     
         ax = plot_figure([a,c],[b,d],[R,r])
         df_plot = df[ df['距離'].isin(['a','b']) ]
         ax.scatter(df_plot.x, df_plot.y, df_plot.z, s=3, c= df_plot.color ,cmap= plt.get_cmap('plasma'),norm = plt.Normalize(vmin=1, vmax=4))
         plt.show()
 
-
+    def printall(self):
+        print(f"My chord is {self.chord}")    
+        print(f"My brace is {self.brace}")
+        
+    def inter(self,theta):
+        a, b, c, d =  self.a, self.b, self.c, self.d
+        R, r  =  self.R, self.r 
+        
+        #座標單位向量
+        Uc = unit_vector(b-a)                               #chord 中心線
+        U_gamma = unit_vector(d-c)                          #brace 中心線
+        U_alpha = unit_vector( np.cross(Uc, U_gamma) )      #chord 中心線 /brace 中心線 公垂線
+        U_beta = unit_vector( np.cross(U_gamma,U_alpha) )   # (brace 平面)
+    
+        beta = np.dot(Uc,U_beta)
+        gamma = np.dot(Uc,U_gamma)
+        #0階
+        C1 = np.dot(c-a , np.cross(U_alpha,Uc)) - r*gamma*np.sin(theta)
+        C2 = R**2 - ( np.dot(c-a,U_alpha) + r*np.cos(theta))**2
+        l = 1/beta *(-C1 + C2**0.5 )
+        PI = c + r*np.cos(theta) * U_alpha + r*np.sin(theta) * U_beta  + l * U_gamma
+        
+        #1階
+        C1_1 = -r*gamma*np.cos(theta)
+        C2_1 = 2*r*np.sin(theta) * np.dot(c-a ,U_alpha) + 2* r**2 * np.cos(theta) * np.sin(theta)
+        l_1 =  1/beta *( -C1_1+C2_1/(2*C2**0.5) )
+        PI_1 = -r*np.sin(theta) * U_alpha + r*np.cos(theta) * U_beta  + l_1 * U_gamma
+        
+        #2階
+        C1_2 = r*gamma*np.sin(theta)
+        C2_2 = 2*r*np.cos(theta) * np.dot(c-a ,U_alpha) + 2* r**2 * ( np.cos(theta)**2 - np.sin(theta)**2 )
+        l_2 =  1/beta * (-C1_2 + 0.5 * C2_2 * C2**-0.5 - 0.25 * C2_1**2 * C2**-1.5)
+        PI_2 = -r*np.cos(theta) * U_alpha - r*np.sin(theta) * U_beta  + l_2 * U_gamma
+        return [PI,PI_1,PI_2]
+    
 # 弦桿中心線
 a = [-1421.00, 16246.00, -40360.00]
 b = [-1421.00, 16246.00,-29660.00]
 R = 1420
 T = 105
 
-# 斜撐中心線
-c = [-2064.00, 14980.00, -34128.00]
-d = [-3709.00, 11079.00, -30614.00]
-r = 717
-t = 32
+# 斜撐中心線1
+c1 = [-2064.00, 14980.00, -34128.00]
+d1 = [-3709.00, 11079.00, -30614.00]
+r1 = 717
+t1 = 32
+
+# 斜撐中心線2
+c2 = [-568.00, 15111.00, -34078.00]
+d2 = [1729.00, 11555.00, -30564.00]
+r2 = 717
+t2 = 32
 
 # 其他
 tw = 10 #焊道腳長
 N = 9 # 每一象限幾等分
 
-
-# 其他
 chord = {'a':a,'b':b,'R':R,'T':T}
-brace = {'c':c,'d':d,'r':r,'t':t}
+brace1 = {'c':c1,'d':d1,'r':r1,'t':t1}
+brace2 = {'c':c2,'d':d2,'r':r2,'t':t2}
 
-p1 = Joints(chord,brace)          
+point_j = Joints(chord,brace1)
+point_k = Joints(chord,brace2)
+
+Newton2d(point_j,point_k,[0,3/2*np.pi], prt_step = True)
---
+++ rotation.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 17:40:12 2022
+
+@author: 63791
+"""
+import numpy as np
+from numpy import norm, cross, dot, array, matrix
+from scipy.spatial.transform import Rotation as R
+
+
+
+def stress_transform(stress_matrix,new_Xasis):
+    
+    def unit_vector(vector):
+        return vector / norm(vector)
+    
+    def cal_axis_angle(vector): 
+        vector = unit_vector( array(vector) )
+        x = array([1,0,0])
+        axis = unit_vector( cross (vector, x) )
+        angle = np.arccos( dot(vector, x) )
+        return axis, angle
+    
+    stress_matrix = matrix( stress_matrix )
+    axis, theta = cal_axis_angle( new_Xasis )
+    rot = R.from_rotvec(theta * axis)
+    transform = rot.as_matrix()
+    
+    return ( transform @ stress_matrix @ transform.T ).round(3)
+    
+    
+# #ex1
+   
+# stress = [[-50,-20, 0],
+#           [-20,-30, 0],
+#           [  0,  0, 0]]
+# stress = np.matrix(stress)
+
+
+# stress_transform(stress,[1,0.6176,0])
+# stress_transform(stress,[1,-0.2364,0])
+
+
+
+# #ex2
+
+# stress = [[-120, 30, 0],
+#           [  30,-60, 0],
+#           [   0,  0, 0]]
+# stress = np.matrix(stress)
+
+# stress_transform(stress,[1,np.tan(-50*np.pi/180),0])
+# stress_transform(stress,[1,-0.2364,0])
